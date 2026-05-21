b3634668f42a97a9289dcf278efbf1860d567377
BUGA <lucas.gabriel2014zl@gmail.com>
2026-05-21 14:36:27 -0300

    fix render port

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("FAC-BOT ONLINE");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor web iniciado na porta ${PORT}`);
+});
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, Partials } = require('discord.js');
 const fs = require('fs');
